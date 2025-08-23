0748644ab472698bd1148a36c63799b3193ef8e6
apjadon7895 <133784566+apjadon7895@users.noreply.github.com>
2025-08-23 14:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (63.74%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int ans =0;
+        int i=1;
+        while(true){
+            if(i%2==0 && i%n==0){
+                ans=i;
+                break;
+            }
+            i++;
+        }
+        return ans;
+        
+    }
+}
